03a550b1ad253ae64b808d1ceb5f48258ab3e3d5
bruno torres araujo <brunotorressaraujo@gmail.com>
2021-03-29 09:50:34 -0300

    Navigation Screens

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import { theme } from './styles/theme';
-import { HomeScreen } from './screens';
+import { Navigator } from './routes';
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar translucent backgroundColor={theme.colors.backgroundDark} />
-      <HomeScreen />
-    </ThemeProvider>
+    <NavigationContainer>
+      <ThemeProvider theme={theme}>
+        <StatusBar translucent backgroundColor={theme.colors.backgroundDark} />
+        <Navigator />
+      </ThemeProvider>
+    </NavigationContainer>
   );
 };
 export default App;
--- src/screens/SplashScreen/index.js
+++ src/screens/SplashScreen/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SectionView, SectionImage } from './styles';
 import { StatusBar } from 'react-native';
 import SplashLogo from '../../assets/img/SevenHousesLogo.png';
+import { useNavigation } from '@react-navigation/native';
 export const SplashScreen = () => {
+  const navigator = useNavigation();
+
+  const goToHomeScreen = () => {
+    setTimeout(() => {
+      navigator.navigate('Home');
+    }, 2000);
+  };
+
+  useEffect(() => {
+    goToHomeScreen();
+  }, []);
+
   return (
     <SectionView>
       <StatusBar translucent backgroundColor="transparent" />
